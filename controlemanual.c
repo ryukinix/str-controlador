f94c6019c45a2608957901cc3d8db395d1023377
Manoel Vilela <manoel_vilela@engineer.com>
2019-05-15 14:33:00 -0300

    Revert controlemanual.c to original state

--- controlemanual.c
+++ controlemanual.c
@@ -58,7 +58,7 @@ void envia_mensagem(int socket_local, struct sockaddr_in endereco_destino, char 
 	/* Envia msg ao servidor */
 
 	if (sendto(socket_local, mensagem, strlen(mensagem)+1, 0, (struct sockaddr *) &endereco_destino, sizeof(endereco_destino)) < 0 )
-	{ 
+	{
 		perror("sendto");
 		return;
 	}
@@ -79,71 +79,9 @@ int recebe_mensagem(int socket_local, char *buffer, int TAM_BUFFER)
 	return bytes_recebidos;
 }
 
-void thread1(){
-  strcpy( msg_enviada, "sta0");
-  strcpy( msg_enviada, "st-0");
-  strcpy( msg_enviada, "sti0");
-  strcpy( msg_enviada, "sno0");
-  strcpy( msg_enviada, "sh-0");
-
-}
-
-void thread2(){
-  	char opcao;
-	do{
-		char teclado[1000];
-		double valor;
-		char msg_enviada[1000];  
-		char msg_recebida[1000];
-		int nrec;
-
-		printf("\n");   //\\Digite a letra da opcao seguida pelo valor, no caso de atuadores:);
-		/*printf("<x> Termina o programa\n");
-		printf("<a> Lê valor de Ta\n");
-		printf("<t> Lê valor de T\n");
-		printf("<i> Lê valor de Ti\n");
-		printf("<o> Lê valor de No\n");
-		printf("<h> Lê valor de H\n");*/
-		printf("<I><valor> Define valor de Ni\n");
-		printf("<Q><valor> Define valor de Q\n");
-		printf("<A><valor> Define valor de Na\n");
-		printf("<F><valor> Define valor de Nf\n"); 
-		printf("Digite a letra da opcao seguida pelo valor, no caso de atuadores:\n");
-
-		fgets( teclado, 1000, stdin);
-		opcao = teclado[0];
-		switch( opcao ) {
-		  	case 'I':	valor = atof( &teclado[1] );
-						sprintf( msg_enviada, "ani%lf", valor);
-						break;
-			case 'Q':	valor = atof( &teclado[1] );
-						sprintf( msg_enviada, "aq-%lf", valor);
-						break;
-			case 'A':	valor = atof( &teclado[1] );
-						sprintf( msg_enviada, "ana%lf", valor);
-						break;
-			case 'F':	valor = atof( &teclado[1] );
-						sprintf( msg_enviada, "anf%lf", valor); 
-						break;
-			default:	printf("Opcao %c nao existe.\n", opcao);
-						continue;
-			}
-
-		printf("Enviado: %s\n", msg_enviada);
-		envia_mensagem(socket_local, endereco_destino, msg_enviada);
-
-		nrec = recebe_mensagem(socket_local, msg_recebida, 1000);
-		msg_recebida[nrec] = '\0';
-		printf("Mensagem de resposta com %d bytes >>>%s<<<\n", nrec, msg_recebida);
-
-	        } while( opcao != 'x' );
-  
-}
-
-
 int main(int argc, char *argv[])
 {
-	if (argc < 3) { 
+	if (argc < 3) {
 		fprintf(stderr,"Uso: controlemanual <endereco> <porta>\n");
 		fprintf(stderr,"<endereco> eh o endereco IP da caldeira\n");
 		fprintf(stderr,"<porta> eh o numero da porta UDP da caldeira\n");
@@ -159,6 +97,64 @@ int main(int argc, char *argv[])
 	struct sockaddr_in endereco_destino = cria_endereco_destino(argv[1], porta_destino);
 
 
+	char opcao;
+	do{
+		char teclado[1000];
+		double valor;
+		char msg_enviada[1000];
+		char msg_recebida[1000];
+		int nrec;
 
+		printf("\n");   //\\Digite a letra da opcao seguida pelo valor, no caso de atuadores:);
+		printf("<x> Termina o programa\n");
+		printf("<a> Lê valor de Ta\n");
+		printf("<t> Lê valor de T\n");
+		printf("<i> Lê valor de Ti\n");
+		printf("<o> Lê valor de No\n");
+		printf("<h> Lê valor de H\n");
+		printf("<I><valor> Define valor de Ni\n");
+		printf("<Q><valor> Define valor de Q\n");
+		printf("<A><valor> Define valor de Na\n");
+		printf("<F><valor> Define valor de Nf\n");
+		printf("Digite a letra da opcao seguida pelo valor, no caso de atuadores:\n");
+
+		fgets( teclado, 1000, stdin);
+		opcao = teclado[0];
+		switch( opcao ) {
+        case 'x':	exit(0);
+        case 'a':	strcpy( msg_enviada, "sta0");
+            break;
+        case 't':	strcpy( msg_enviada, "st-0");
+            break;
+        case 'i':	strcpy( msg_enviada, "sti0");
+            break;
+        case 'o':	strcpy( msg_enviada, "sno0");
+            break;
+        case 'h':	strcpy( msg_enviada, "sh-0");
+            break;
+        case 'I':	valor = atof( &teclado[1] );
+            sprintf( msg_enviada, "ani%lf", valor);
+            break;
+        case 'Q':	valor = atof( &teclado[1] );
+            sprintf( msg_enviada, "aq-%lf", valor);
+            break;
+        case 'A':	valor = atof( &teclado[1] );
+            sprintf( msg_enviada, "ana%lf", valor);
+            break;
+        case 'F':	valor = atof( &teclado[1] );
+            sprintf( msg_enviada, "anf%lf", valor);
+            break;
+        default:	printf("Opcao %c nao existe.\n", opcao);
+            continue;
+        }
+
+		printf("Enviado: %s\n", msg_enviada);
+		envia_mensagem(socket_local, endereco_destino, msg_enviada);
+
+		nrec = recebe_mensagem(socket_local, msg_recebida, 1000);
+		msg_recebida[nrec] = '\0';
+		printf("Mensagem de resposta com %d bytes >>>%s<<<\n", nrec, msg_recebida);
+
+	} while( opcao != 'x' );
 
 }
